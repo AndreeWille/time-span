97c236c27698c8f6738094afcb32c021bce5b4ab
Andree Wille <AndreeWille@users.noreply.github.com>
2020-01-17 13:43:28 +0100

    add initial project created by stagehand

---
+++ example/time_warp_example.dart
@@ -0,0 +1,6 @@
+import 'package:time_warp/time_warp.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/time_warp.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library time_warp;
+
+export 'src/time_warp_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
