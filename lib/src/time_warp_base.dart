60e778f6face71ce14953f0a95dab175ac311d9d
Andree Wille <AndreeWille@users.noreply.github.com>
2020-01-21 21:30:55 +0100

    add TimeSpan class

--- lib/src/time_warp_base.dart
+++ lib/src/time_warp_base.dart
@@ -1,6 +1,18 @@
-// TODO: Put public facing types in this file.
+import 'package:time_parser/time_parser.dart';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+abstract class TimeSpan {
+  Duration startTime;
+  Duration endTime;
+  
+  double get span { 
+    return ((endTime - startTime).inMinutes) / 60;
+  }
+  
+  TimeSpan(String startTime, String endTime) {
+    var beginning = TimeParser.parse(startTime);
+    var end = TimeParser.parse(endTime);
+
+    this.startTime = Duration(hours: beginning.hours, minutes: beginning.minutes);
+    this.endTime   = Duration(hours: end.hours, minutes: end.minutes);
+  }
 }
